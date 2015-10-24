ac28148e57df88983bdad7d3a9c72677db50fe53
Dongle Su <sudl@etuo.cn>
2015-10-24 10:19:31 +0800

    remove bash modify.Course it cause bash not working.

--- init/30_osx_homebrew_recipes.sh
+++ init/30_osx_homebrew_recipes.sh
@@ -23,14 +23,3 @@ if [[ "$(type -P $binroot/htop)" ]] && [[ "$(stat -L -f "%Su:%Sg" "$binroot/htop
   sudo chown root:wheel "$binroot/htop"
   sudo chmod u+s "$binroot/htop"
 fi
-
-# bash
-if [[ "$(type -P $binroot/bash)" && "$(cat /etc/shells | grep -q "$binroot/bash")" ]]; then
-  e_header "Adding $binroot/bash to the list of acceptable shells"
-  echo "$binroot/bash" | sudo tee -a /etc/shells >/dev/null
-fi
-if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/bash" ]]; then
-  e_header "Making $binroot/bash your default shell"
-  sudo chsh -s "$binroot/bash" "$USER" >/dev/null 2>&1
-  e_arrow "Please exit and restart all your shells."
-fi
